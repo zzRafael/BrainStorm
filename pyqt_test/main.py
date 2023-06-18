b2457ecd03747fb565563c077bc5e736b9cb609b
Rafael Alves <citygotham552@gmail.com>
2023-06-17 22:07:38 -0300

    Some tests

--- pyqt_test/main.py
+++ pyqt_test/main.py
@@ -1,54 +1,26 @@
-def create(id):
-    list.append(id)
+from PyQt6 import QtWidgets
+from PyQt6.QtWidgets import QApplication, QMainWindow
+import sys
 
-def read(id):
-    print(id)
-
-def update(id, new_id):
-    list.remove(id)
-    list.append(new_id)
-
-def delete(id):
-    try:
-        for i in range(len(list)):
-            list.remove(id)
-    except ValueError:
-        pass
-
-title = 'System'
-char = '='
-char_num = 10
-
-list = []
-
-print(char * char_num,' System ', char * char_num)
-
-while True:
-    option = input('Escolha uma opção: c=criar, r=ler, u=atualizar, d=deletar, s=sair: ')
-    if option[0] == 'c':
-        created_id = input('Informe o novo ID para cadastro: ')
-        create(created_id)
-
-    if option[0] == 'r':
-        print("======List======")
-        if len(list) < 1:
-          print('A lista está vazia. Digite "c" para adicionar mais IDs.')
-        else:
-            for item in list:
-                print(item)
-        print("======List======")
-
-    if option[0] == 'u':
-        old_id = input('Informe a ANTIGO ID: ')
-        new_id = input('Informe a NOVO ID: ')
-        update(old_id, new_id)
-
-    if option[0] == 'd':
-        to_delete = input('Digite o ID que deseja deletar: ')
-        delete(to_delete)
+class MyWindow(QMainWindow):
     
-    if option[0] == 's':
-        break
 
+def clicked():
+    print('Cliked!')
 
-print(char * char_num,' System foi fechado ', char * char_num)
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(200,200,600,400)
+    win.setWindowTitle('UniSystem')
+
+    for i in range(10):
+        b1 = QtWidgets.QPushButton(win)
+        b1.setText('Ok')
+        b1.move(i*100,10)
+        b1.clicked.connect(clicked)
+
+    win.show()
+    sys.exit(app.exec())
+
+window()
