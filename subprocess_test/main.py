9b26166d2e56b36349e5e95de5035886d7d7cb52
Rafael Alves <citygotham552@gmail.com>
2023-07-04 22:43:50 -0300

    Update main.py

--- subprocess_test/main.py
+++ subprocess_test/main.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def executor():
-    subprocess.Popen("C:\\Users\\User\\Documents\\GitHub\\BrainStorm\\subprocess_test\\exe\\dist\\counter.exe")
+    subprocess.call(["python", "C:\Users\User\Documents\GitHub\BrainStorm\subprocess_test\exe\dist\counter.exe"])
 
 exe_number = 5
 
