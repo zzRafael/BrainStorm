a02d8ae330a773551f8fa0d98c993db4607eb098
Rafael Alves <102750561+zzRafael@users.noreply.github.com>
2023-07-28 17:25:05 -0300

    Update requests_test.py

--- PySimpleGUI_test/requests_test.py
+++ PySimpleGUI_test/requests_test.py
@@ -55,33 +55,57 @@ while True:
 
 
 import PySimpleGUI as sg
+from random import choice
+import pyautogui
 headings = ['Name', 'Size', 'Type']
 
-sizes = ['Small', 'Medium', 'Big']
+sizes = ['Polimento', 'Cravando', 'Flash']
 types = ['Rocky', 'Gaseous', 'Frozen', 'Desertic']
 
-data = []
+data_base = []
 
+width, height = pyautogui.size()
+print(width)
+print(height)
 
+frame_size = (width // 3, height - 200)
 
 layout = [
     [
-    sg.Frame('',[[sg.Table(data,headings = headings,justification = 'left',max_col_width = 500,expand_x = True,expand_y = True,auto_size_columns = True,key = '_MAIN_TABLE_')]], expand_x=True, expand_y=True),
-    sg.Frame('',[[sg.Text('TELA_1', key='_MAIN_TEXT_')], [sg.Input(key='_MAIN_INPUT_')], [sg.Button('Try', key='_MAIN_BUTTON_', expand_x = True)]], expand_x=True, expand_y=True)
-    ]
-]
-    
-window = sg.Window('TEST WINDOW', layout, resizable=True, finalize=True)
+    sg.Frame('',[[sg.Text('UNISYSTEM')]], size = frame_size, expand_y = True),
+    sg.Frame('',[[sg.Table(data_base,headings = headings,justification = 'left',max_col_width = 500,expand_x = True,expand_y = True,auto_size_columns = True,key = '_MAIN_TABLE_')]], size = frame_size, expand_y = True),
+    sg.Frame('',[[sg.Text('Add new orders', key = '_STATUS_TEXT_')], [sg.Input(key = '_MAIN_INPUT_', do_not_clear = False)], [sg.Button('ADD', key = '_SUBMIT_BUTTON_')]], element_justification = 'center', size = frame_size, expand_y = True),
+    ]]
+sg.Input(())
 
+sg.theme('DarkTeal9')
+window = sg.Window('TEST WINDOW', layout, resizable=True, finalize=True)
+window.Maximize()
 
 while True:
-    window.Maximize()
     event, values = window.read()
+    
     if event == sg.WIN_CLOSED:
         break
-    if event == '_MAIN_BUTTON_':
-        data.append([f"{values['_MAIN_INPUT_']}", choice(sizes),choice(types)])
-        window['_MAIN_TEXT_'].update(f"{values['_MAIN_INPUT_']}")
-        window['_MAIN_TABLE_'].update(data)
+    
+    if event == '_SUBMIT_BUTTON_':
+        print('botao ok')
+        size_ = choice(sizes)
+        type_ = choice(types)
+        name = values['_MAIN_INPUT_']
+        data_base.append([f"{name}", size_, type_])
+                
+        window['_MAIN_TABLE_'].update(data_base)
+        
+        for data in data_base:
+            if data[1] == 'Polimento':
+                color = 'green'
+            if data[1] == 'Cravando':
+                color = 'yellow'
+            if data[1] == 'Flash':
+                color = 'red'
+
+            window['_MAIN_TABLE_'].Update(row_colors = [[data_base.index(data), color]])
+        
     print(f'EVENT: {event}\nVALUE: {values}')
 
